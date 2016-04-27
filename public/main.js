188798d70a0bdf1d8f4b7db320923ff853cfbad8
valmassoi <rvalmassoi@gmail.com>
2016-04-27 13:07:38 -0700

    redirect working

--- public/main.js
+++ public/main.js
@@ -1,112 +1,124 @@
-//TODO BABEL
+'use strict' //TODO BABEL
 
-var path = require('path')
-var express = require('express')
-var http = require('http')
-var shortid = require('shortid')
-var mongo = require('mongodb').MongoClient
+const path = require('path')
+const express = require('express')
+const http = require('http')
+const shortid = require('shortid')
+const mongo = require('mongodb').MongoClient
 
-var app = express()
+const app = express()
 
 const dbUrl = 'mongodb://localhost:27017/data' //change PORT
 
-
-function checkUrl(){
-
+function checkUrl(url){
+  let regexp = /^(https?):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  return regexp.test(url);
 }
 
-function shorten(input){
-
-  checkUrl(input) //TODO if input is good
+function shorten(origUrl, callback){
 
   //create id
-  var id = shortid.generate()//TODO  //testing with xxx
+  let id = shortid.generate()//TODO  //testing with xxx
   console.log(id)
 
-
-  var item = {
-    "original_url": input,
+  let item = {
+    "original_url": origUrl,
     "id": id
-  } //Make sure you use JSON.stringify convert it to JSON.
+  }
 
-  mongo.connect(dbUrl, function(err, db) { //mongo db input and id   CHECK IF USED
+  mongo.connect(dbUrl, (err, db) => { //mongo db origUrl and id   CHECK IF USED
     if (err) throw err
-    var urls = db.collection('urls')
+    let urls = db.collection('urls')
     urls.find({ //TODO CHECK BOTH ID AND url
       "original_url": {
-        $eq: input
+        $eq: origUrl
       }
-    }).toArray(function(err, exists) {
+    }).toArray((err, exists) => {
       if (err) throw err
       if (exists.length != 0){
-        console.log("tryagain")
-        console.log(exists)
+        console.log("orig url alread exists, its id was: "+exists[0].id)
+        callback(exists[0].id)
+        db.close()
       }
       else{
-        urls.insert(item, function(err, data) {// if not in docs
+        urls.insert(item, (err, data) => {// if not in docs
           if (err) throw err
           console.log(JSON.stringify(item))
+          callback(id)
           db.close()
         })
       }
+
     })
   })
-
-  return id
-
 }
 
-function checkDb(shorturl){
+function checkDb(shorturl, callback){
   console.log(shorturl);
-  mongo.connect(dbUrl, function(err, db) {
+  mongo.connect(dbUrl, (err, db) => {
     if (err) throw err
-    var urls = db.collection('urls')
+    let urls = db.collection('urls') //move to top
     urls.find({ //CHECK
-      "id": {
-        $eq: shorturl
-      }
-    }).toArray(function(err, exists) {
+      id: {
+          $eq: shorturl
+        }
+      }).toArray((err, orig) => {
       if (err) throw err
-      if (exists.length != 0){
-        console.log("return true");
-        return true;
+      if (orig.length != 0){
+        console.log("orig url:" + orig[0].original_url);
+        callback(orig[0].original_url)//callback("http://" + orig[0].original_url)
+        db.close()
       }
-      if (exists.length == 0){
+      if (orig.length == 0){
         console.log("dont exist fam");
-        return false;
+        db.close()
+        callback(0);
       }
     })
   })
 }
 
-app.all("*", function(req, res, next) {
+app.all("*", (req, res, next) => {
   next()
 });
-app.get("/:shorturl", function(req, res) {
-  var shorturl = req.params.shorturl
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname })
+});
+app.get("/:shorturl", (req, res) => {
+  let shorturl = req.params.shorturl
   //if in db redirect
-  if (checkDb(shorturl)){
-    console.log("redirect");
-    res.redirect("http://www.apple.com")//TODO Change to longurl from db
-  }
-  else{
-    console.log("no redirect");
-    res.end()
-  }
+  checkDb(shorturl, (url) => {
+    if (url!=0){
+      console.log("redirect");
+      res.redirect(url)//TODO Change to longurl from db
+    }
+    else{
+      res.writeHead(200, { "Content-Type": "application/json" });
+      let json = JSON.stringify({"error" : "Short URL does not exist, please create one --> minime.herokuapp.com/new/http://yoururl.com"})
+      res.end(json)
+    }
+  })
 });
-
-app.get("/new/:origUrl", function(req, res) {
+app.get("/new/*", (req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
-  var origUrl = req.params.origUrl
-  var shortUrl = "minime.herokuapp.com/" + shorten(origUrl)//TODO make url dynamic
-  var json = JSON.stringify({"original_url": origUrl, "short_url": shortUrl})
-  res.end(json)
+  let origUrl = req.params[0]
+
+  if (checkUrl(origUrl)){
+    shorten(origUrl, (id) => {
+      console.log(id)
+      let shortUrl = "minime.herokuapp.com/" + id //TODO make first part of url dynamic for forks
+      let json = JSON.stringify({"original_url": origUrl, "short_url": shortUrl})
+      res.end(json)
+    })
+  }
+  else {
+    let json = JSON.stringify({"error" : "Bad URL format, make sure it contains http:// or https://"})
+    res.end(json)
+  }
+
 });
 
-app.get("*", function(req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("404!")
-});
-var port = process.env.PORT || 8080;
+
+const port = process.env.PORT || 8080;
 http.createServer(app).listen(port)
-console.log("Server Running on port:" + port)
+console.log("Server Running on port: " + port)
